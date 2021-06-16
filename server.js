9a5915fa21b5720032f923b71256648315097561
MIRIAM SILVA <MIRIAM-SILVA@LIVE.COM>
2021-06-16 10:09:03 -0700

    added weather info

--- server.js
+++ server.js
@@ -1,58 +1,54 @@
+//Node tester
 console.log('hello from node!');
 
-const { response, request } = require('express');
-//use require instead of import. 
-//the next two line will ALWAYS look like this
 const express = require('express');
 const app = express();
 
-//use this to access our .env file
 require('dotenv').config();
 
-//allow the front end to access the server  
 const cors = require('cors');
 app.use(cors());
-//make port a global variable
-const PORT = process.env.PORT;
 
+const PORT = process.env.PORT;
+//--------------------------------------------------------------
 //what route our server should be listening for
-const weatherData = require('./data/weather.json')
+const weatherData = require('./data/weather.json');
 
 //Tester
-app.get('/potato',(request,response)=>{
-  response.send('potatoes are delicious!');
+app.get('/',(req,res)=>{
+  res.send('This is working!');
 });
 
-//Weather
-app.get('/weather', (request, response)=>{
-  let cityName = request.query.name;
-  let cityLon = request.query.lon;
-  let cityLat = request.query.lat;
+//API endpoint
+app.get('/weather', (req,res)=>{
+  let lat= req.query.lat;
+  let lon = req.query.lon;
+  let searchQuery = req.query.searchQuery;
 
-  weatherData.find(idx =>{
-    if(cityName.toLowerCase() !== idx.city_name.toLowerCase()){
-      response.send('could not find the request')
-    }
-  })
+  console.log(lat, lon,searchQuery);
+
+  //use .find()
+  let cityWeatherData = weatherData.find(city=> city.city_name === searchQuery);
+  console.log (cityWeatherData);
+  //error message
+  if (cityWeatherData === undefined){
+    res.status(400).send('unsupported city');
+    //if no error message show city
+  }else{
+    let cityDataPetrified = cityWeatherData.data.map(obj=> new Forecast(obj.datetime,`Low of ${obj.low_temp}, high of ${obj.max_temp}, with ${obj.weather.description.toLowerCase()}`));
+    res.send(cityDataPetrified);
+  }
 })
 
-//class Forecast
+//class forecast w/ date & description as parameters
 class Forecast{
-  constructor(description,date){
-    this.description = description;
+  constructor(date,description){
     this.date = date;
+    this.description = description;
   }
 }
 
-//Forecast
-cityForecast = () => {
-  weatherData.map(i=>{
-    new Forecast(i.description, i.date);
-  });
-};
-cityForecast();
-
-// this /* will listen for anything. PUT IT LAST
+//Catch All Error Message
 app.get('/*', (request, response)=>{
   response.status(404).send('Sorry, We Only Have Information on Seattle, Amman & Paris, Please Try Again!')
 });
